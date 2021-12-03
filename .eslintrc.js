3f8b57a839822f4866c02be15cd640037ce49a1e
Thistleman <codes5096@gmail.com>
2021-12-03 09:33:41 -0800

    Set up working directories and file structures, installed needed modules

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'linebreak-style': 0,
+    'import/extensions': ['error', 'ignorePackages'],
+  },
+};
